8a6fb67117cd8446a814abfbb6ac2517c04b21c2
yasoe31 <yasoe31@gmail.com>
2015-08-12 22:32:26 +0100

    added function get a customer

--- src/main/java/com/rga/customermodule/controller/CustomerController.java
+++ src/main/java/com/rga/customermodule/controller/CustomerController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -39,6 +40,13 @@ public class CustomerController {
 
 		LOGGER.debug("Registering A Customer");
 		customerService.save(customer);
+	}
+	
+	@RequestMapping(value="/getACustomer/{id}",method=RequestMethod.GET)
+	@ResponseBody
+	private Customer getACustomer(@PathVariable String id) {
+		
+		return customerService.findOne(Integer.parseInt(id));
 	}
 
 }
