642747a17835a63bc53451f6c8933edc852f30f7
yasoe31 <yasoe31@gmail.com>
2015-08-12 22:06:26 +0100

    re-formatted

--- src/main/java/com/rga/customermodule/service/CustomerService.java
+++ src/main/java/com/rga/customermodule/service/CustomerService.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Service;
 
 import com.rga.customermodule.model.Customer;
 
-
-
 @Service
 public interface CustomerService extends CrudRepository<Customer, Integer> {
 
